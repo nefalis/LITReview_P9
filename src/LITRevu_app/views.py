7e0025d2b6e6090a9d316220c5a8aa5ad6fcdc85
nefalis <nefalis@gmail.com>
2024-06-26 17:11:32 +0200

    connexion et enregistrement utilisateur

--- src/LITRevu_app/views.py
+++ src/LITRevu_app/views.py
@@ -1,14 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .models import Ticket, Review, UserFollows
 
 
-def login_view(request):
-    return render(request, "home.html")
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
 
-def signup_view(request):
-    return render(request, "signup.html")
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("flux")
+        else:
+            messages.info(request, "Identifiant ou mot de passe incorrect")
+
+    form = AuthenticationForm()
+    return render(request, "home.html", {"form": form})
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Votre compte a été créé avec succès.")
+            return redirect("flux")
+    else:
+        form = UserCreationForm()
+    
+    return render(request, "register.html", {"form": form})
+
+def logout_user(request):
+    logout(request)
+    return redirect("login")
+
 
 def flux_view(request):
-    return render(request, "flux.html")
+    context = {"tickets" : Ticket.objects.all()}
+    return render(request, "flux.html", context)
 
 def suscribes_view(request):
     return render(request, "suscribes.html")
